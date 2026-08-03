f882e23cb6c82d144ae4bacfd057bb49619c36e7
LSanwith <1689292034@qq.com>
2026-08-04 01:19:40 +0800

    Update generateConfig.js

--- generateConfig.js
+++ generateConfig.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 const { register_anonimous } = require('./main')
-const { cookieToJson, generateRandomChineseIP } = require('./util/index')
-const { getXeapiPublicKey } = require('./util/xeapiKey')
+const { getXeapiPublicKey } = require('./util/xeapiKey')   // 这个依赖保留
 const tmpPath = require('os').tmpdir()
+
+// 内联随机中国IP生成（代替 generateRandomChineseIP）
+function generateRandomChineseIP() {
+  const prefixes = ['116', '117', '118', '119', '120', '121', '122', '123', '124', '125', '126', '127', '220', '221', '222', '223'];
+  const a = prefixes[Math.floor(Math.random() * prefixes.length)];
+  const b = Math.floor(Math.random() * 256);
+  const c = Math.floor(Math.random() * 256);
+  const d = Math.floor(Math.random() * 254) + 1;
+  return `${a}.${b}.${c}.${d}`;
+}
 
 async function generateConfig() {
   global.cnIp = generateRandomChineseIP()
@@ -11,7 +20,7 @@ async function generateConfig() {
     const res = await register_anonimous()
     const cookie = res.body.cookie
     if (cookie) {
-      const cookieObj = cookieToJson(cookie)
+      const cookieObj = require('./util/index').cookieToJson(cookie)  // 用到 cookieToJson 时再 require
       fs.writeFileSync(
         path.resolve(tmpPath, 'anonymous_token'),
         cookieObj.MUSIC_A,
